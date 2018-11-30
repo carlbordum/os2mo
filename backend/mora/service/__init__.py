c527812cfbc8bf94435f1788b77267c0e0ccc2e6
Robert Jensen <jensen.robert@gmail.com>
2018-11-30 08:54:03 +0000

    First work towards user-editable configuration

--- backend/mora/service/__init__.py
+++ backend/mora/service/__init__.py
@@ -23,6 +23,7 @@ from . import orgunit
 from . import related  # noqa
 from . import role  # noqa
 from . import integration_data
+from . import configuration_options
 
 blueprints = (
     address.blueprint,
@@ -37,4 +38,7 @@ blueprints = (
     org.blueprint,
     orgunit.blueprint,
     related.blueprint,
+    exports.blueprint,
+    integration_data.blueprint,
+    configuration_options.blueprint,
 )
